32a5b8a3ee8de2d61643c409dd31ad3caf953e58
SanyamSaini <sanyamsaini51@gmail.com>
2021-06-20 01:52:32 +0530

    Integrated UI code for Weather Activity and the recycler view item.

--- app/src/main/java/com/sanyam/jetpack/example/WeatherActivity.kt
+++ app/src/main/java/com/sanyam/jetpack/example/WeatherActivity.kt
@@ -3,9 +3,9 @@ package com.sanyam.jetpack.example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WeatherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_weather)
     }
 }
