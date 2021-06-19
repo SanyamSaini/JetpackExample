fac910c934df0ebf98b0146269a64cf56494bb87
SanyamSaini <sanyamsaini51@gmail.com>
2021-06-20 02:20:29 +0530

    Integrated 'View binding' for main layout.

--- app/src/main/java/com/sanyam/jetpack/example/WeatherActivity.kt
+++ app/src/main/java/com/sanyam/jetpack/example/WeatherActivity.kt
@@ -2,10 +2,16 @@ package com.sanyam.jetpack.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sanyam.jetpack.example.databinding.ActivityWeatherBinding
 
 class WeatherActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWeatherBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_weather)
+        binding = ActivityWeatherBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
